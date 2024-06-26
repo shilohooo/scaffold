e5e691353c9e13a7a0a266c7d3dd1b9f486d7a03
shiloh <lixiaolei595@gmail.com>
2024-06-26 23:34:44 +0800

    refactor: 添加 mybatis plus 列注解

--- cuckooji-backend/cuckooji-common/src/main/java/com/cuckooji/common/core/domain/BaseEntity.java
+++ cuckooji-backend/cuckooji-common/src/main/java/com/cuckooji/common/core/domain/BaseEntity.java
@@ -1,8 +1,8 @@
 package com.cuckooji.common.core.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.cuckooji.common.constant.DatePatternConstants;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +10,7 @@ import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,41 +25,45 @@ public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 搜索值
+     * 创建人 ID
+     * <p>
+     * fixme：改为 Long 类型
      */
-    @JsonIgnore
-    private String searchValue;
-
-    /**
-     * 创建者
-     */
+    @TableField("create_by")
     private String createBy;
 
     /**
      * 创建时间
      */
+    @TableField("create_time")
     @JsonFormat(pattern = DatePatternConstants.NORM_DATETIME_PATTERN)
     private Date createTime;
 
     /**
-     * 更新者
+     * 更新人 ID
+     * <p>
+     * fixme：改为 Long 类型
      */
+    @TableField("update_by")
     private String updateBy;
 
     /**
      * 更新时间
      */
+    @TableField("update_time")
     @JsonFormat(pattern = DatePatternConstants.NORM_DATETIME_PATTERN)
     private Date updateTime;
 
     /**
      * 备注
      */
+    @TableField("remark")
     private String remark;
 
     /**
      * 请求参数
      */
+    @TableField(exist = false)
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private Map<String, Object> params;
+    private Map<String, Object> params = new HashMap<>(0);
 }
